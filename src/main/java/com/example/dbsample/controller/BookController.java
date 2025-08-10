622df9daa5ab899aed8fbb5f2c8ac599aab312ec
kazuuud <jinmakazuhiro@outlook.jp>
2025-08-10 20:13:12 +0900

    書籍一覧表示機能の一時的な無効化: BookControllerからshowAllBooksメソッドを削除し、success.htmlの書籍表示部分を削除

--- src/main/java/com/example/dbsample/controller/BookController.java
+++ src/main/java/com/example/dbsample/controller/BookController.java
@@ -1,34 +1,16 @@
 package com.example.dbsample.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.example.dbsample.mapper.BookMapper;
-
-import lombok.RequiredArgsConstructor;
-
 
 
 @Controller
-@RequiredArgsConstructor
 public class BookController {
-
-    // DI
-    private final BookMapper bookMapper;
 
     // メニュー画面表示
     @GetMapping("/")
     public String showIndex(){
         return "book/index";
-    }
-
-    // 書籍一覧表示
-    @GetMapping("/list")
-    public String showAllBooks(Model model){
-        model.addAttribute("message","一覧表示");
-        model.addAttribute("books", bookMapper.getAllBooks());
-        return "book/success";
     }
 
 }
