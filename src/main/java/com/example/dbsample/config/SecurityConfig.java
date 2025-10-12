9d7289b5cd389cd10d7589d9f607e227eb4cf7e1
KAZUHIRO JINMA <k-jinma@uhe.ac.jp>
2025-10-12 20:17:36 +0900

    Spring Bootのバージョンを3.5.6に更新し、セキュリティ設定を追加しました。また、404エラーページを新規作成し、管理者のみが書籍登録リンクにアクセスできるようにしました。

---
+++ src/main/java/com/example/dbsample/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.dbsample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  // パスワードの暗号化
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  InMemoryUserDetailsManager userDetailsService() {
+    // 管理者設定
+    UserDetails admin = User
+        .withUsername("admin")
+        .password(passwordEncoder().encode("admin"))
+        .roles("ADMIN")
+        .build();
+
+    // ユーザー設定
+    UserDetails user = User
+        .withUsername("user")
+        .password(passwordEncoder().encode("user"))
+        .roles("USER")
+        .build();
+    return new InMemoryUserDetailsManager(admin, user);
+  }
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.formLogin(login -> login //  フォーム認証を使う
+        .permitAll())
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers("/css/**").permitAll() // CSSファイルは認証不要で使えるようにする
+            //.requestMatchers("/").permitAll() //  トップページは認証不要
+            .anyRequest().authenticated() //  他のURLはログイン後アクセス可能
+        );
+
+    return http.build();
+  }
+
+}
